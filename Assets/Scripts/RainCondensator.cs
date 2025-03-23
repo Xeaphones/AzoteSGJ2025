40d7cbc16125ac3f8b8b523874e43f0fbfe37129
NoahDelc <noah.delcourt.974@gmail.com>
2025-03-23 10:46:01 +0100

    Add cloud animation

--- Assets/Scripts/RainCondensator.cs
+++ Assets/Scripts/RainCondensator.cs
@@ -4,29 +4,68 @@ public class RainCondensator : CleaningUnit
 {
     [SerializeField] public bool isActive;
     [SerializeField] private float timer;
-    [SerializeField] public int activationTics;
+    //[SerializeField] public int activationTics;
+    [SerializeField] private float firstRainStartDelay;
+    [SerializeField] private float rainDuration;
+    [SerializeField] private float rainCooldown;
 
     [SerializeField] new public Collider2D collider;
+    [SerializeField] GameObject clouds;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        isActive = true;
+        isActive = false;
+        timer = rainCooldown - firstRainStartDelay;
     }
 
     // Update is called once per frame
     new void Update()
     {
         base.Update();
-        if (timer >= activationTics)
+        timer += Time.deltaTime;
+        if(isActive)
         {
-            collider.enabled = !collider.enabled;
-            timer = 0;
-            Debug.Log(collider.enabled);
+            if(timer > rainDuration) 
+            {
+                timer = 0;
+                StopRain();
+            }
         }
         else
         {
-            timer += Time.deltaTime;
+            if(timer > rainCooldown) 
+            {
+                timer = 0;
+                ActivateRain();
+            }
         }
+
+
+
+        // if (timer >= activationTics)
+        // {
+        //     collider.enabled = !collider.enabled;
+        //     timer = 0;
+        //     Debug.Log(collider.enabled);
+        // }
+        // else
+        // {
+        //     timer += Time.deltaTime;
+        // }
+    }
+
+    void ActivateRain()
+    {
+        isActive = true;
+        clouds.SetActive(true);
+        collider.enabled = true;
+    }
+
+    void StopRain()
+    {
+        isActive = false;
+        clouds.SetActive(false);
+        collider.enabled = false;
     }
 }
