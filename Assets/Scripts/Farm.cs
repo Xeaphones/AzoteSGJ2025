a3090da43a89092fa86a2d23847782e66b87d9eb
Eramoth <axelleref@gmail.com>
2025-03-23 01:47:30 +0100

    fire no spread on farm ; removed useless fire.OnCreate()

--- Assets/Scripts/Farm.cs
+++ Assets/Scripts/Farm.cs
@@ -2,5 +2,57 @@ using UnityEngine;
 
 public class Farm : PollutingUnit
 {
+    [SerializeField] public bool onFire = false;
+    [SerializeField] public float maxHp;
+    [SerializeField] public float currentHp = 0;
+    [SerializeField] public GameObject firePrefab;
+    private bool hasFireOn = false;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        currentHp = maxHp;
+    }
+
+    new void Update()
+    {
+        base.Update();
+        if (currentHp <= 0)
+        {
+            Destroy(gameObject);
+        }
+        else if (currentHp <= maxHp - 2 & !hasFireOn)
+        {
+            hasFireOn = true;
+            GameObject newFire = Instantiate (firePrefab, transform.position, transform.rotation);
+        }
+
+        // when onFire, loose hp
+        if (onFire)
+        {
+            currentHp -= Time.deltaTime;
+            Debug.Log("forest is loosing hp due to fire ");
+        }
+        
+    }
+
+    void OnTriggerStay2D(Collider2D collision)
+    {
+        GameObject other = collision.gameObject;
+
+        if (other.GetComponent<Fire>() & onFire == false)
+        {
+            onFire = true;
+            if (other.transform.position == transform.position)
+            {
+                hasFireOn = true;
+            }
+        }
+        else if (other.GetComponent<RainCondensator>() & onFire == true)
+        {
+            onFire = false;
+            hasFireOn = false;
+            currentHp = maxHp;
+        }
+    }
 }
--- Assets/Scripts/Fire.cs
+++ Assets/Scripts/Fire.cs
@@ -6,12 +6,6 @@ public class Fire : PollutingUnit
     [SerializeField] public float duration = 10;
     private Unit source;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
-    
-    public void OnCreation(Unit unit)
-    {
-        source = unit;
-    }
-
     void Start()
     {
         
